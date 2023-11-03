ea9d19b0dd0069e1e66ffe1422e5bcb66a3e2796
shun <shun@shunnoMacBook-Pro.local>
2023-11-03 17:02:21 +0900

    imple chat page

--- app/api/openai/[slug]/route.ts
+++ app/api/openai/[slug]/route.ts
@@ -5,8 +5,12 @@ export async function GET(request: Request, { params }) {
   const openai = new OpenAI()
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "" },
+    ],
     model: "gpt-4",
+    temperature: 0,
   })
 
   console.log(completion.choices[0])
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react"
 
 interface Message {
@@ -6,7 +7,7 @@ interface Message {
   sender: "user" | "bot"
 }
 
-export default function ChatPage() {
+export default function ChatClientComponent() {
   const [messages, setMessages] = useState<Message[]>([])
   const [inputText, setInputText] = useState("")
 
@@ -14,30 +15,38 @@ export default function ChatPage() {
     e.preventDefault()
     if (inputText.trim() === "") return
 
-    setMessages([
-      ...messages,
-      { id: messages.length, text: inputText, sender: "user" },
-    ])
+    if (messages.length > 0) {
+      setMessages((pre) => [
+        ...pre,
+        { id: pre.length, text: inputText, sender: "user" },
+      ])
+    } else {
+      setMessages((pre) => [{ id: 1, text: inputText, sender: "user" }])
+    }
+
     setInputText("")
   }
 
   return (
     <div>
       <div className="chat-box">
-        {messages.map((msg) => (
-          <div key={msg.id} className={msg.sender}>
+        {messages?.map((msg) => (
+          <div key={msg.id} className={`message ${msg.sender}`}>
             {msg.text}
           </div>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={inputText}
-          onChange={(e) => setInputText(e.target.value)}
-        />
-        <button type="submit">送信</button>
-      </form>
+      <div className="input-box">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={inputText}
+            onChange={(e) => setInputText(e.target.value)}
+            placeholder="メッセージを入力"
+          />
+          <button type="submit">送信</button>
+        </form>
+      </div>
     </div>
   )
 }
